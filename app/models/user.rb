378dbcb3991495c47f3c36bb956f315dbd890ce8
David Rodgers <dtrodger20@gmail.com>
2016-10-21 11:26:03 -0500

    AJAX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,12 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
-  # has_many :posts, foreign_key: :author_id
-  # has_many :votes, foreign_key: :voter_id
-
-  # validates :username, :email, :password_hash, presence: true
-  # validate :validate_password
 
   def validate_password
     if @raw_password.nil?
       errors.add(:password_hash, "is required")
     elsif @raw_password.length < 8
       errors.add(:password_hash, "must be 8 characters or more")
-    # elsif @raw_password != /([!@#$%^&*?\d]+.+|.+[!@#$%^&*?\d]+.+|.+[!@#$%^&*?\d])/
-    #   errors.add(:password_hash, 'must contain a special character or number')
     end
   end
 
